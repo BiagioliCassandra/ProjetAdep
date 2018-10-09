6e3f3304737e7c4cde0c1d8520a714832e8ab9a5
Vincent <vincentrichardeau@laposte.net>
2018-10-09 18:07:16 +0200

    html footer

---
+++ footer.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+ 
+<footer>
+  <div class="logo">
+    <img src="https://www.adep-roubaix.fr/img/roubaix.png" alt="ville de Roubaix">
+    <img src="https://www.adep-roubaix.fr/img/fse.jpeg" alt="slogan europe">
+    <img src="https://www.adep-roubaix.fr/img/europe.jpg" alt="union européene">
+    <img src="https://www.adep-roubaix.fr/img/cget.png" alt="cget">
+    <img src="https://www.adep-roubaix.fr/img/prefet.jpg" alt="prefet">
+    <img src="https://www.adep-roubaix.fr/img/region.png" alt="région">
+    <img src="https://www.adep-roubaix.fr/img/nord.png" alt="nord département">
+    <img src="https://www.adep-roubaix.fr/img/mel.png" alt="MEL">
+    <img src="https://www.adep-roubaix.fr/img/ecole-numerique.png" alt="Ecole numérique">
+    <img src="https://www.adep-roubaix.fr/img/arofesep.png" alt="Arofesep">
+    <img src="https://www.adep-roubaix.fr/img/logo_esaat.png" alt="Esaat">
+    <img src="https://www.adep-roubaix.fr/img/simplon.png" alt="simplon">
+    <img src="https://www.adep-roubaix.fr/img/adep-logo.png" alt="adep">
+  </div>
+  <div class="copyright">
+    <p>copyright 2018 - <a href="mentions légales"></a></p>
+  </div>
+</footer>
+  <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+
+</html>
