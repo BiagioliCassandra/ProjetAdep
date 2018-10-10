4bc4432931d7231bc33ee8b1d9bf15ef50303b43
Vincent <vincentrichardeau@laposte.net>
2018-10-10 11:19:16 +0200

    finish footer php

--- footer.php
+++ footer.php
@@ -1,6 +1,3 @@
-<!doctype html>
-<html class="no-js" lang="">
-
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -11,49 +8,54 @@
   <link rel="manifest" href="site.webmanifest">
   <link rel="apple-touch-icon" href="icon.png">
   <!-- Place favicon.ico in the root directory -->
-
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/main.css">
+  <!-- FONT -->
+  <link rel="stylesheet" href="font/css/font-awesome.min.css">
 </head>
 
 <body>
  
 <footer>
   <div class="logo">
-    <img src="https://www.adep-roubaix.fr/img/roubaix.png" alt="ville de Roubaix">
-    <img src="https://www.adep-roubaix.fr/img/fse.jpeg" alt="slogan europe">
-    <img src="https://www.adep-roubaix.fr/img/europe.jpg" alt="union européene">
-    <img src="https://www.adep-roubaix.fr/img/cget.png" alt="cget">
-    <img src="https://www.adep-roubaix.fr/img/prefet.jpg" alt="prefet">
-    <img src="https://www.adep-roubaix.fr/img/region.png" alt="région">
-    <img src="https://www.adep-roubaix.fr/img/nord.png" alt="nord département">
-    <img src="https://www.adep-roubaix.fr/img/mel.png" alt="MEL">
-    <img src="https://www.adep-roubaix.fr/img/ecole-numerique.png" alt="Ecole numérique">
-    <img src="https://www.adep-roubaix.fr/img/arofesep.png" alt="Arofesep">
-    <img src="https://www.adep-roubaix.fr/img/logo_esaat.png" alt="Esaat">
-    <img src="https://www.adep-roubaix.fr/img/simplon.png" alt="simplon">
-    <img src="https://www.adep-roubaix.fr/img/adep-logo.png" alt="adep">
-  </div>
+    <div>
+      <img src="https://www.adep-roubaix.fr/img/roubaix.png" alt="ville de Roubaix">
+    </div>
+    <div>
+      <img src="https://www.adep-roubaix.fr/img/fse.jpeg" alt="slogan europe">
+    </div> 
+    <div>
+      <img src="https://www.adep-roubaix.fr/img/europe.jpg" alt="union européene">
+    </div>
+    <div>
+      <img src="https://www.adep-roubaix.fr/img/cget.png" alt="cget">
+    </div>
+    <div>
+      <img src="https://www.adep-roubaix.fr/img/prefet.jpg" alt="prefet">
+    </div>
+    <div>
+      <img src="https://www.adep-roubaix.fr/img/region.png" alt="région">
+    </div>
+    <div>
+      <img src="https://www.adep-roubaix.fr/img/nord.png" alt="nord département">
+    </div>
+    <div>
+      <img src="https://www.adep-roubaix.fr/img/mel.png" alt="MEL">
+    </div>
+    <div>
+      <img src="https://www.adep-roubaix.fr/img/ecole-numerique.png" alt="Ecole numérique">
+    </div>
+    <div>
+      <img src="https://www.adep-roubaix.fr/img/arofesep.png" alt="Arofesep">
+    </div>
+    <div>
+      <img src="https://www.adep-roubaix.fr/img/logo_esaat.png" alt="Esaat">
+    </div>
+    <div> 
+      <img src="https://www.adep-roubaix.fr/img/simplon.png" alt="simplon">
+    </div>
+    </div>
   <div class="copyright">
-    <p>copyright 2018 - <a href="mentions légales"></a></p>
+    <p><i class="fa fa-copyright" aria-hidden="true"> copyright 2018</i> - <a href="mentions légales">mentions légales</a></p>
   </div>
 </footer>
-  <!--[if lte IE 9]>
-    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-  <![endif]-->
-
-  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
-  <script src="js/plugins.js"></script>
-  <script src="js/main.js"></script>
-
-  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-  <script>
-    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-    ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
-  </script>
-  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
-</body>
-
-</html>
